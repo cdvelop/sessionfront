bc0da06717efc66d37b9eb192cf320ff9a09f6f5
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-03 00:04:58 -0300

    update module: model

--- go.mod
+++ go.mod
@@ -2,15 +2,15 @@ module github.com/cdvelop/sessionfrontend
 
 go 1.20
 
-require github.com/cdvelop/model v0.0.74
+require github.com/cdvelop/model v0.0.75
 
-require github.com/cdvelop/sessionhandler v0.0.1
+require github.com/cdvelop/sessionhandler v0.0.2
 
 require (
-	github.com/cdvelop/input v0.0.57 // indirect
-	github.com/cdvelop/object v0.0.38 // indirect
+	github.com/cdvelop/input v0.0.59 // indirect
+	github.com/cdvelop/object v0.0.39 // indirect
 	github.com/cdvelop/strings v0.0.7 // indirect
-	github.com/cdvelop/timetools v0.0.23 // indirect
+	github.com/cdvelop/timetools v0.0.24 // indirect
 )
 
 replace github.com/cdvelop/model => ../model
--- submit-login.go
+++ submit-login.go
@@ -1,10 +1,56 @@
 package sessionfrontend
 
-import "syscall/js"
+import (
+	"syscall/js"
+
+	"github.com/cdvelop/model"
+)
 
 func (s *sessionFrontend) submitLoginForm(this js.Value, btn []js.Value) interface{} {
 
-	s.Log("boton login clikeado:", btn[0])
+	s.Log("boton login clikeado")
+
+	s.Log("DATA:", s.Form.FormData)
+
+	s.Log(s.Session_status)
+
+	// s.CreateObjectsInDB(s.Table, true, map[string]string{
+	// 	s.Id_session:     "123",
+	// 	s.Session_status: "in",
+	// 	s.Session_encode: "xx",
+	// })
+
+	s.ReadStringDataAsyncInDB(model.ReadDBParams{
+		FROM_TABLE:      s.Table,
+		WHERE:           []string{s.Session_status},
+		SEARCH_ARGUMENT: "in",
+	}, func(result []map[string]string, err string) {
+		if err != "" {
+			s.UserMessage(err)
+			return
+		}
+
+		if len(result) == 1 {
+			s.Log("hay usuario en db local:", result)
+
+		} else {
+			s.Log("no hay usuario en local. enviando data al backend:", s.Form.FormData)
+
+			s.SendOneRequest("POST", "create", s.Form.ObjectName, s.Form.FormData, func(result []map[string]string, err string) {
+
+				if err != "" {
+					s.Log(err)
+					return
+				}
+
+				s.Log("RESULTADO SESIÓN:", result)
+
+			})
+
+		}
+
+	})
+	// form_name := f.html_form.Get("name").String()
 
 	return nil
 
