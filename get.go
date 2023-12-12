14af411f904b23bd9d8a941b0714b38a130c21a3
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-11 23:05:37 -0300

    fix frontend chec user

--- get.go
+++ get.go
@@ -6,5 +6,10 @@ import (
 
 func (s *sessionFrontend) FrontendCheckUser(result func(u *model.User, err string)) {
 
+	if s.current_session == nil {
+		result(&model.User{}, "error no hay usuario registrado")
+		return
+	}
+
 	result(s.DecodeUser(s.current_session.Session_encode))
 }
